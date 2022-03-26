8fc35f8acc19e362cc5341f5150ff7fdadd99282
Zasholle <200223256str@gmail.com>
2022-03-26 23:28:45 +0300

    adding element and page base classes

---
+++ MailTesting/WebObjects/BaseElement.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Drawing;
+using MailTesting.WebDriver;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace MailTesting.WebObjects
+{
+    public class BaseElement : IWebElement
+    {
+        private readonly IWebDriver _driver = Browser.GetDriver();
+        protected string Name;
+        protected By Locator;
+        protected IWebElement Element;
+
+        public BaseElement(By locator, string name)
+        {
+            Locator = locator;
+            Name = name == "" ? GetText() : name;
+        }
+
+        public string GetText()
+        {
+            WaitForIsVisible();
+            return Element.Text;
+        }
+
+        public IWebElement GetElement()
+        {
+            try
+            {
+                Element = Browser.GetDriver().FindElement(Locator);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return Element;
+        }
+
+        public void WaitForIsVisible()
+        {
+            var wait = new WebDriverWait(_driver, new TimeSpan(0,0,30));
+
+            wait.Until(condition =>
+            {
+                try
+                {
+                    var elementToBeDisplayed = _driver.FindElement(Locator);
+                    return elementToBeDisplayed.Displayed;
+                }
+                catch (StaleElementReferenceException)
+                {
+                    return false;
+                }
+                catch (NoSuchElementException)
+                {
+                    return false;
+                }
+            });
+        }
+
+        public IWebElement FindElement(By @by)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ReadOnlyCollection<IWebElement> FindElements(By @by)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendKeys(string text)
+        {
+            WaitForIsVisible();
+            Browser.GetDriver().FindElement(Locator).SendKeys(text);
+        }
+
+        public void Submit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Click()
+        {
+            WaitForIsVisible();
+            Browser.GetDriver().FindElement(Locator).Click();
+        }
+
+        public string GetAttribute(string attributeName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetDomAttribute(string attributeName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetProperty(string propertyName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetDomProperty(string propertyName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetCssValue(string propertyName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISearchContext GetShadowRoot()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string TagName { get; }
+        public string Text { get; }
+        public bool Enabled { get; }
+        public bool Selected { get; }
+        public Point Location { get; }
+        public Size Size { get; }
+        public bool Displayed { get; }
+    }
+}
---
+++ MailTesting/WebObjects/BasePage.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+
+namespace MailTesting.WebObjects
+{
+    public class BasePage
+    {
+        protected By TitleLocator;
+        protected string Title;
+        public static string TitleForm;
+
+        protected BasePage(By titleLocator, string title)
+        {
+            TitleLocator = titleLocator;
+            Title = TitleForm = title;
+            AssertIsOpen();
+        }
+
+        public void AssertIsOpen()
+        {
+            var label = new BaseElement(TitleLocator, Title);
+            label.WaitForIsVisible();
+        }
+    }
+}
