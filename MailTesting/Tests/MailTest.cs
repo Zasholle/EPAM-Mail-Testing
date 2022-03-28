bcab982d257847cfe36f5e691c31c5cae2a833e4
Zasholle <200223256str@gmail.com>
2022-03-28 04:21:13 +0300

    Adding tests

---
+++ MailTesting/Tests/MailTest.cs
@@ -0,0 +1,37 @@
+﻿using MailTesting.WebObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MailTesting.Tests
+{
+    [TestClass]
+    public class MailTest : BaseTest
+    {
+        private HomePage _homePage;
+        private MailBoxPage _mailBoxPage;
+
+        [TestMethod]
+        public void MessageCreationTest()
+        {
+            _homePage = new HomePage();
+            _homePage.SignIn();
+
+            _mailBoxPage = new MailBoxPage();
+            _mailBoxPage.CreateMessage();
+            _mailBoxPage.LogOut();
+        }
+
+        [TestMethod]
+        public void DraftDeletionTest()
+        {
+            _homePage = new HomePage();
+            _homePage.SignIn();
+
+            _mailBoxPage = new MailBoxPage();
+
+            if (_mailBoxPage.CheckDraft())
+                _mailBoxPage.DeleteDraft();
+
+            _mailBoxPage.LogOut();
+        }
+    }
+}
